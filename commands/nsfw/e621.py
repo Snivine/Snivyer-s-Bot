622679b936a6eb42e67b53465d419eb88f408688
Snivyer1910 <snivyer1910@gmail.com>
2021-03-15 04:56:26 -0600

    Can't find an API, leaving as is and saving

---
+++ commands/nsfw/e621.py
@@ -0,0 +1,21 @@
+#Imports
+import discord
+from discord.ext import commands
+#import insert e621 api here
+
+#ples end me I can't find API
+
+#Actual command
+
+class e621(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(description="Fetches a post from e621")
+    async def e621(self, ctx, *, content):
+        if ctx.channel.is_nsfw():
+            ctx.send("This is a NSFW-only Command")
+        elif content == "":
+            ctx.send("You have to input a set of tags to search")
+        else:
+
