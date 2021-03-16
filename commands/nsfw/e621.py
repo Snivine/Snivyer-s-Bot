ec83a04fd6b460ad06d8212a49ecc1544c8a8ed8
Snivyer1910 <snivyer1910@gmail.com>
2021-03-15 21:41:53 -0600

    3rd test command, definetly final

--- commands/nsfw/e621.py
+++ commands/nsfw/e621.py
@@ -3,8 +3,7 @@ import discord
 from discord.ext import commands
 import json
 import requests
-
-#ples end me I can't find API
+from random import randint
 
 #Actual command
 
@@ -17,20 +16,24 @@ class e621(commands.Cog):
         if not ctx.channel.is_nsfw():
            await ctx.send("This is a NSFW-only Command")
         else:
+            randpage = randint(1, 30)
+            randpost = randint(0, 15)
             tags = content.split(",")
             url = "https://e621.net/posts.json?tags="
 
             for t in tags:
                 url += t + "+"
 
-            url += "&max=5&page=1"
+            url += f"&max=16&page={randpage}"
 
             user_agent = { "User-agent": "Snivyer's Maid" }
 
             print(url)
             res = requests.get(url, headers = user_agent)
             posts = json.loads(res.text)
-            await ctx.send(posts["posts"][0]["file"]["url"])
+            await ctx.send(posts["posts"][randpost]["file"]["url"])
 
 def setup(bot):
     bot.add_cog(e621(bot))
+
+#fixed some shit and made it random, thanks to my friendo to help me with this
