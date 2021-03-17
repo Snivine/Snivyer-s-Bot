dc8a561322d059c64351c8e15118bd87558516a5
Snivyer1910 <snivyer1910@gmail.com>
2021-03-17 00:36:09 -0600

    Made Memey stuff more unlikely and also added an alternative meme message

--- commands/fun/gay.py
+++ commands/fun/gay.py
@@ -2,18 +2,14 @@
 #imports
 ###
 
-import discord
 from discord.ext import commands
-from random import randint
 import random
-import asyncio
 
 ###
 #random gen
 ###
 
 emoji = "🏳️‍🌈"
-
 
 ###
 #acutal command
@@ -25,14 +21,21 @@ class gay(commands.Cog):
 
     @commands.command(description="Fetch the ammount of gayness of a person")
     async def gay(self, ctx, *, arg=None):
-        possibilites = [randint(0, 100)] * 90 + ["999"] * 4 + ["255"] * 4 + ["`yes.`"] * 1
-        gaynumber = random.choice(possibilites)
+        messagetypes = ["Normal"] * 9999 + ["Meme"] * 1
+        randommessage = random.choice(messagetypes) #This took me more than it needed to.
+        # no, really, this shit doesn't accept floats and I was trying to use floats
 
-        # If no argument passed, set the phrase to "You are"
-        phrase = "You are" if arg is None else f"{arg} is"
+        if randommessage == "Meme":
+            await ctx.send("Idk man ask them lmao") #suggested by Floofy Foxor and MrRhino
+        else:
+             possibilites = [random.randint(0, 100)] * 9799 + ["999"] * 100 + ["255"] * 100 + ["Yes."] * 1
+             gaynumber = random.choice(possibilites) #Tried a lot of methods to get this to work, then noticed I'm stupid
 
-        await ctx.send(f"{phrase} {gaynumber}% gay {emoji}")
-        print(f"Generated gay number: {gaynumber}")
+             # If no argument passed, set the phrase to "You are"
+             phrase = "You are" if arg is None else f"{arg} is"
+
+             await ctx.send(f"{phrase} {gaynumber}% gay {emoji}")
+             print(f"Generated gay number: {gaynumber}")
 
 ###
 # Cog
@@ -45,4 +48,4 @@ def setup(bot):
 #Notes
 ###
 
-#THIS IS DEFINETLY NOT PRODUCTION READY, SINCE THIS IS A HACKY WORKAROUND, EXPECT BUGS
+#Reperak bad developer /s
