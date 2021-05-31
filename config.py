32f1e654941ae9566bb97b510e1df8a0d385b756
Snivyer1910 <snivyer1910@gmail.com>
2021-05-31 04:22:21 -0600

    Unhardoce e621 User Angent

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ __botdetails = __parser.add_argument_group('bot details')
 __botdetails.add_argument('-t', '--token', type=str, help='token for the bot')
 __botdetails.add_argument('-p', '--prefix', type=str, default='%', help='prefix for the bot')
 __botdetails.add_argument("-gh", "--ghtoken", type=str, help="github token")
+__botdetails.add_argument("-e6u", "--e621useragent", type=str, help="e621 user agent")
 
 # Tuning configuration (e.g. disabling sharding)
 __bottuning = __parser.add_argument_group('bot tuning')
